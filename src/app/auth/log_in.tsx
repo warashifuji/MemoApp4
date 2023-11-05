386f1c205cea91b15d13b42ac702ad5cf7a1f70c
fujiwara <shuhei.22wara@gmail.com>
2023-11-05 18:41:09 +0900

    firebase以外

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -3,6 +3,7 @@ import {
 } from 'react-native'
 
 import { Link, router } from 'expo-router'
+import { useState } from 'react'
 
 import Button from '../../components/Button'
 const handlePress = (): void => {
@@ -11,12 +12,30 @@ const handlePress = (): void => {
 }
 
 const LogIn = (): JSX.Element => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <View style={styles.container}>
       <View style={styles.inner}>
         <Text style={styles.title}>Log In</Text>
-        <TextInput style={styles.input} value ='Email address' />
-        <TextInput style={styles.input} value ='Password' />
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={(text) => { setEmail(text) }}
+          autoCapitalize='none'
+          keyboardType='email-address'
+          placeholder='Email Address'
+          textContentType='emailAddress'
+        />
+        <TextInput
+          style={styles.input}
+          value = {password}
+          onChangeText={(text) => { setPassword(text) }}
+          autoCapitalize='none'
+          secureTextEntry
+          placeholder='Oassword'
+          textContentType='password'
+        />
         <Button label='Submit' onPress={handlePress} />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Not registerd?</Text>
