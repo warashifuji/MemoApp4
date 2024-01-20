d5a3cf68a3c7b6ee49a451727ac9a4b69811e832
warashifuji <shuhei.22wara@gmail.com>
2024-01-21 05:24:41 +0900

    レイアウト tsx->jsx

---
+++ src/app/_layout.jsx
@@ -0,0 +1,22 @@
+import { Stack } from "expo-router"
+
+const Layout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#467fD3"
+        },
+        headerTintColor: "white",
+        headerTitle: "MemoApp",
+        headerBackTitle: "Back",
+        headerTitleStyle: {
+          fontSize: 22,
+          fontWeight: "bold"
+        }
+      }}
+    />
+  )
+}
+
+export default Layout
